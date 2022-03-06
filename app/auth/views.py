7eac3fdfcefce687b2675c8051d2a972b303e76b
tracy <tracyjacobs775@gmail.com>
2022-03-06 21:27:11 +0300

    add text

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to pitch","email/welcome_user",user.email,user=user)
+        mail_message("Welcome! ","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
